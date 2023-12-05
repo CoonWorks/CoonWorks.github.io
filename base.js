250a726bba3d841c7d55977f9cda3463340512a5
José Ignacio Bravo <josebravo_2009@hotmail.com>
2023-12-05 09:54:37 -0300

    Create base.js

---
+++ base.js
@@ -0,0 +1,20 @@
+const rando = document.getElementById('boton');
+const texto = document.getElementById('prueba');
+
+function randomizer(){
+  min = Math.ceil(0);
+  max = Math.floor(16777215);
+  resultado = Math.floor(Math.random() * (max - min) + min)
+  pasoAHex = resultado.toString(16);
+  for (i = 0; i <= 6; i++){
+    if(pasoAHex.length < i){
+      pasoAHex = "0" + pasoAHex;
+    }
+  }
+  texto.value = "#"+ pasoAHex;
+  document.body.style.backgroundColor = texto.value;
+}
+
+randomizer();
+
+rando.addEventListener("click",randomizer);
